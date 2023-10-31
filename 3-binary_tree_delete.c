ed51779e9dd9ece7c3e2b80be7d12fefdb701944
gladysdo <doe.glad@gmail.com>
2023-10-30 17:59:47 -0700

    Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - it is recursively traversing and deleting all nodes
+ * @tree: pointer to the root
+ * Return: 0
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return;
+	}
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
+}
