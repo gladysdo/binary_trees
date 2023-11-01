95367c628dfc7a6826bd736325ba1187006333af
gladysdo <doe.glad@gmail.com>
2023-10-31 22:23:36 -0700

    Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -9,13 +9,14 @@
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	size_t size = 0;
+
+	if (tree)
 	{
-		return (0);
+		size += 1;
+		size += binary_tree_size(tree->left);
+		size += binary_tree_size(tree->right);
+
 	}
-
-	size_t left_size = binary_tree_size(tree->left);
-	size_t right_size = binary_tree_size(tree->right);
-
-	return (left_size + right_size + 1);
+	return (size);
 }
